7d5b8b1be780816916a8d6d32cd1a60f880063a2
hugocg21 <hugocastanogonzalez@gmail.com>
2024-07-22 02:51:25 +0200

    Created components: "chat-list", "chat-room", "login", and "register"; and the "auth" service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ChatListComponent } from './components/chat-list/chat-list.component';
+import { ChatRoomComponent } from './components/chat-room/chat-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ChatListComponent,
+    ChatRoomComponent
   ],
   imports: [
     BrowserModule,
